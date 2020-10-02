5386ad1492a62d82edacab475dd82f44758b383b
hkm007 <hkm0072001@gmail.com>
2020-10-02 18:29:45 +0530

    user schema virtuals, methods created

--- models/user.js
+++ models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const crypto = require('crypto');
+const uuidv1 = require('uuid/v1');
 
 const userSchema = mongoose.Schema({
     username: {
@@ -13,9 +15,9 @@ const userSchema = mongoose.Schema({
         unique: true,
         trim: true
     },
-    password: {
+    encry_password: {
         type: String,
-        trim: true
+        required: true
     },
     salt: String,
     info: {
@@ -31,5 +33,29 @@ const userSchema = mongoose.Schema({
         default: []
     }
 })
+
+userSchema.virtual("password")
+        .set(function(password) {
+            this._password = password;
+            this.salt = uuidv1();
+            this.encry_password = this.securePassword(password);
+        })
+        .get(function() {
+            return this._password;
+        })
+
+userSchema.method = {
+    securePassword: function(plainpassword) {
+        if(!plainpassword) return "";
+        try {
+            return crypto.createHmac('sha256', this.salt).update(plainpassword).digest('hex');
+        } catch(err) {
+            return "";
+        }
+    },
+    authenticate: function(plainpassword) {
+        return this.securePassword(plainpassword) === this.encry_password;
+    }
+}
 
 module.exports = mongoose.model("User", userSchema);
