b48b9778643647bc908bde58e550603e4b0a444a
hkm007 <hkm0072001@gmail.com>
2020-10-02 18:59:03 +0530

    product schema created

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const crypto = require('crypto');
 const uuidv1 = require('uuid/v1');
 
-const userSchema = mongoose.Schema({
+const userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
