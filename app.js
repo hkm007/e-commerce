9331164600835a32301f079c516522aca5ebeae1
hkm007 <hkm0072001@gmail.com>
2020-10-02 22:33:24 +0530

    mongodb connected

---
+++ app.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGOURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongoose.connection.on('connected', () => {
+    console.log("mongodb connected...");
+});
+
+mongoose.connection.on('error', (err) => {
+    console.log("Error in connecting to database", err);
+});
+
+app.listen(PORT, () => console.log(`server running on port ${PORT}`));
