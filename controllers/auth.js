dc563faf202b3bdd916f9b7078a8b9d48efd9604
hkm007 <hkm0072001@gmail.com>
2020-10-03 01:30:05 +0530

    isAuthenticated and isAdmin middleware done

--- controllers/auth.js
+++ controllers/auth.js
@@ -73,3 +73,24 @@ exports.isSignedIn = expressJwt({
 })
 
 // custom middleware
+exports.isAuthenticated = (req, res, next) => {
+    let checker = req.profile && req.auth && req.profile._id === req.auth._id;
+    
+    if(!checker) {
+        return res.status(403).json({
+            error: "Access Denied"
+        })
+    }
+
+    next();
+}
+
+exports.isAdmin = (req, res, next) => {
+    if(req.profile.role === 0) {
+        return res.status(403).json({
+            error: "You are not admin, access denied!"
+        })
+    }
+
+    next();
+}
